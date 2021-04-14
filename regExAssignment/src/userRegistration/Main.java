ef41a99120473d2ae9d41a815d0159338ebb3e8e
testForMe-Now <81403199+testForMe-Now@users.noreply.github.com>
2021-04-14 20:30:53 +0530

    Update Main.java

--- regExAssignment/src/userRegistration/Main.java
+++ regExAssignment/src/userRegistration/Main.java
@@ -4,29 +4,11 @@ import java.util.*;
 
 public class Main{
     public static void main(String args[]) {
-
-
-      Scanner sc = new Scanner(System.in);
-		
-	  userFirstName FirstName = new userFirstName();
-      String firstname = sc.nextLine();
-      System.out.println(FirstName + ": "+ FirstName.isValidFirstName(firstname));
-	  
-      userLastName LastName = new userLastName();
-      String lastname = sc.nextLine();
-      System.out.println(lastname + ": "+ LastName.isValidLastName(lastname));
-	  
-	  userEmail Email = new userEmail();
-      String email = sc.nextLine();
-      System.out.println(email + ": " + Email.isValidEmail(email));
-	  
-	  userMobileNumber MobileNumber = new userMobileNumber();
-      String mobilenumber = sc.nextLine();
-      System.out.println(mobilenumber + ": "+ MobileNumber.isValidMobileNumber(mobilenumber));
-	  
-	  passwordUC4 password = new passwordUC4();
-      String Password = sc.nextLine();
-      System.out.println(Password + ": "+ password.isValidFirstName(Password));
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter the user password");
+	passwordUC4 password = new passwordUC4();
+  	String Password = sc.nextLine();
+    	System.out.println(Password + ": "+ password.isValidFirstName(Password));
 	  
     }
 }
