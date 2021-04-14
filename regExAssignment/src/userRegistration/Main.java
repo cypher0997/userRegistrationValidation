bfd215c2e64e1725776997bb7a0df1cc885ec7a5
testForMe-Now <abcodehash@gmail.com>
2021-04-14 10:59:09 +0530

    password validation added

--- regExAssignment/src/userRegistration/Main.java
+++ regExAssignment/src/userRegistration/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main{
     public static void main(String args[]) {
 
-		  System.out.println("welcome to userRegister");
+
       Scanner sc = new Scanner(System.in);
 		
 	  userFirstName FirstName = new userFirstName();
