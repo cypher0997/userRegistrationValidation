255ed019d59c4921473401e94aff04964d3d09d9
testForMe-Now <81403199+testForMe-Now@users.noreply.github.com>
2021-04-14 20:32:06 +0530

    Update Main.java

--- regExAssignment/src/userRegistration/Main.java
+++ regExAssignment/src/userRegistration/Main.java
@@ -4,13 +4,11 @@ import java.util.*;
 
 public class Main{
     public static void main(String args[]) {
-
-		  System.out.println("welcome to userRegister");
-      Scanner sc = new Scanner(System.in);
-	    
-	 userMobileNumber MobileNumber = new userMobileNumber();
-      String mobilenumber = sc.nextLine();
-      System.out.println(mobilenumber + ": "+ MobileNumber.isValidMobileNumber(mobilenumber));
+     	Scanner sc = new Scanner(System.in);
+	userMobileNumber MobileNumber = new userMobileNumber();
+	System.out.println("enter the user mobilenumber");
+      	String mobilenumber = sc.nextLine();
+      	System.out.println(mobilenumber + ": "+ MobileNumber.isValidMobileNumber(mobilenumber));
 	  
     }
 }
