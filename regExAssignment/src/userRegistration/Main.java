5df145dc690f39ef2b1ecd49930a57ea91d729cf
testForMe-Now <81403199+testForMe-Now@users.noreply.github.com>
2021-04-14 20:33:55 +0530

    Update Main.java

--- regExAssignment/src/userRegistration/Main.java
+++ regExAssignment/src/userRegistration/Main.java
@@ -4,12 +4,10 @@ import java.util.*;
 
 public class Main{
     public static void main(String args[]) {
-
-		  System.out.println("welcome to userRegister");
-      Scanner sc = new Scanner(System.in);
-
-      userFirstName FirstName = new userFirstName();
-      String firstname = sc.nextLine();
-      System.out.println(FirstName + ": "+ FirstName.isValidFirstName(firstname));
+      	Scanner sc = new Scanner(System.in);
+	System.out.println("enter user firstname");
+      	userFirstName FirstName = new userFirstName();
+      	String firstname = sc.nextLine();
+      	System.out.println(FirstName + ": "+ FirstName.isValidFirstName(firstname));
     }
 }
