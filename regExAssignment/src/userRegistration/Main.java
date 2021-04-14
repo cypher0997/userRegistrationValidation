5ea6b893ed9b1611791181bb4d05e332164ae7a0
testForMe-Now <81403199+testForMe-Now@users.noreply.github.com>
2021-04-14 10:35:29 +0530

    Update Main.java

--- regExAssignment/src/userRegistration/Main.java
+++ regExAssignment/src/userRegistration/Main.java
@@ -11,18 +11,5 @@ public class Main{
       userFirstName FirstName = new userFirstName();
       String firstname = sc.nextLine();
       System.out.println(FirstName + ": "+ FirstName.isValidFirstName(firstname));
-
-      userEmail Email = new userEmail();
-      String email = sc.nextLine();
-      System.out.println(email + ": " + Email.isValidEmail(email));
-
-      userMobileNumber MobileNumber = new userMobileNumber();
-      String mobilenumber = sc.nextLine();
-      System.out.println(mobilenumber + ": "+ MobileNumber.isValidMobileNumber(mobilenumber));
-
-      passwordUC4 password = new passwordUC4();
-      String Password = sc.nextLine();
-      System.out.println(Password + ": "+ password.isValidFirstName(Password));
-
     }
 }
